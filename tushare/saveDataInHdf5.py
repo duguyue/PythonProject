4e5257f8a7ca6017c0a3a1e58662f6d030ebcdd1
lindl <lin_donglian@163.com>
2017-05-18 13:46:05 +0800

    tushare获取股票数据

--- tushare/saveDataInHdf5.py
+++ tushare/saveDataInHdf5.py
@@ -1,12 +1,23 @@
 import numpy as np
 import pandas as pd
-
+import tushare as ts
+"""
 a=np.random.standard_normal((9,4))
 b=pd.DataFrame(a)
 b.columns=[['num1','num2','num3','num4']]
 
-#print(b)
-
 h5=pd.HDFStore('/data/stock/test1.h5','w')
 h5['data']=b
 h5.close()
+"""
+
+'''
+df=ts.get_k_data('002743')
+print(df)
+df.to_hdf('e:/day/hdf.h5','002743')
+'''
+
+df=ts.get_hist_data('000875')
+store=HDFStore('e:/day/store.h5')
+store['000875']=df
+store.close()
